0f9e55a1f85a03ac648d3aec093c6f121d45b8f6
chaowei <geekw@foxmail.com>
2016-07-27 16:19:33 +0800

    initial commit

---
+++ Demo/FDDebugObserverDemo/FDDebugObserverDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FDDebugObserverDemo
+//
+//  Created by weichao on 16/7/27.
+//  Copyright © 2016年 chaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/FDDebugObserverDemo/FDDebugObserverDemo/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  FDDebugObserverDemo
+//
+//  Created by weichao on 16/7/27.
+//  Copyright © 2016年 chaowei. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "FDDebugObserver.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)changeValue:(id)sender {
+    [UIApplication sharedApplication].idleTimerDisabled = ![UIApplication sharedApplication].idleTimerDisabled;
+}
+
+- (IBAction)add:(id)sender {
+    [[FDDebugObserver fd_sharedDebugObserver] fd_addObservedObject:[UIApplication sharedApplication] observedKeyPath:@"idleTimerDisabled"];
+}
+
+- (IBAction)remove:(id)sender {
+    [[FDDebugObserver fd_sharedDebugObserver] fd_removeObservedObject:[UIApplication sharedApplication] observedKeyPath:@"idleTimerDisabled"];
+}
+- (IBAction)log:(id)sender {
+    [[FDDebugObserver fd_sharedDebugObserver] fd_logCurrentobserverDictionary];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
